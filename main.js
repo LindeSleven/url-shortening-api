3c8587e88ed2e14d84a48ee2ae2d8d7040bed997
Linde <linde.sleven@gmail.com>
2020-06-08 11:27:54 +0200

    add media queries

--- main.js
+++ main.js
@@ -14,21 +14,19 @@ const error = document.querySelector(".error");
 const btn = document.querySelector("#btn-link");
 const results = document.querySelector(".shorten-links");
 
+let savedLinks = [];
+loadLinks();
+
 const createLink = async (link) => {
   try {
     const response = await axios.post("https://rel.ink/api/links/", {
       url: link,
     });
-    //const hashId = response.data.hashid;
-    //const shortLink = `https://rel.ink/${hashId}`;
-    //return shortLink;
     return response.data;
   } catch (error) {
     return "Not a valid URL";
   }
 };
-
-const linkDiv = [];
 
 btn.addEventListener("click", async () => {
   if (link.value === "") {
@@ -37,16 +35,18 @@ btn.addEventListener("click", async () => {
   } else {
     error.classList.remove("show");
     link.classList.remove("show");
+
     const linkData = await createLink(link.value);
-    linkDiv.push(linkData);
-    console.log(linkDiv);
-    for (let link of linkDiv) {
-      results.innerHTML = linkTemplate(link);
-    }
+    results.innerHTML += linkTemplate(linkData);
+
+    savedLinks.push(linkData);
+    localStorage.setItem("savedLinks", JSON.stringify(savedLinks));
+
+    link.value = "";
   }
 });
 
-const linkTemplate = (linkData) => {
+function linkTemplate(linkData) {
   const { hashid, url, created_at } = linkData;
   const shortLink = `https://rel.ink/${hashid}`;
   return `
@@ -56,25 +56,15 @@ const linkTemplate = (linkData) => {
       <button id="btn-copy" class="btn btn-rectangle">Copy</button>
     </div>
   `;
-};
+}
 
-// -------------
-
-/*const createLink = async (data) => {
-  try {
-    const { hashid, url, created_at } = data;
-    console.log(hashid);
-    const response = await axios.get("https://rel.ink/api/links/");
-    console.log(response);
-    return response;
-  } catch (error) {
-    console.log(error);
+function loadLinks() {
+  savedLinks = JSON.parse(localStorage.getItem("savedLinks"));
+  if (savedLinks !== null) {
+    for (let link of savedLinks) {
+      results.innerHTML += linkTemplate(link);
+    }
+  } else {
+    savedLinks = [];
   }
-};
-
-const fetchNewLink = async (link) => {
-  const newLink = await createHash(link);
-  //let newLink = await createLink(hashLink);
-  //console.log(newLink);
-  return newLink;
-};*/
+}
